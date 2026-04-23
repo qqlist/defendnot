1de979cb9dea826314877dfaeaebb8d12c712a89
qqlist <46162555+qqlist@users.noreply.github.com>
2026-04-23 17:45:12 +0800

    Update strings.hpp
    中文支持

--- cxx-shared/shared/strings.hpp
+++ cxx-shared/shared/strings.hpp
@@ -7,7 +7,7 @@ namespace strings {
     constexpr std::string_view kRepoUrl = "https://github.com/es3n1n/defendnot";
     constexpr std::string_view kVersion = "1.5.0";
     constexpr std::string_view kRepoUrl = "https://github.com/es3n1n/defendnot";
-    constexpr std::wstring_view kDefaultAVName = L"FengJie";
+    constexpr std::wstring_view kDefaultAVName = L"dnot.sh";
     constexpr std::string_view kVictimProcess = "Taskmgr.exe";
     constexpr std::string_view kDllName = "defendnot.dll";
     constexpr std::string_view kWSCUnavailableError = /// !winserver
